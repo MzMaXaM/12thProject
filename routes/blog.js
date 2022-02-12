1bd507c6b43b76586899887ae78c4c926a6517ac
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-02-12 20:58:02 +0000

    we can open post details

--- routes/blog.js
+++ routes/blog.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 router.get('/posts', async (req, res) => {
   const query = `
   SELECT posts.*, authors.name AS author FROM posts 
-  INNER JOIN authors ON posts.author_id = author_id`
+  INNER JOIN authors ON posts.author_id = authors.id`
   const [posts] = await db.query(query)
   res.render('posts-list', {
     posts: posts
@@ -34,6 +34,29 @@ router.get('/new-post', async (req, res) => {
   const [authors] = await db.query('SELECT * FROM authors')
   res.render('create-post', {
     authors: authors
+  })
+})
+
+router.get('/posts/:id', async (req, res) => {
+  const query = `
+  SELECT posts.*, authors.name AS authorName, Authors.email AS authorEmail
+  FROM posts INNER JOIN authors ON posts.author_id = authors.id
+  WHERE posts.id = ?`
+  const [post] = await db.query(query, [req.params.id])
+
+  if (!post || post.length == 0) {
+    return res.status(404).render('404')
+  }
+
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const postData = {
+    ...post[0],
+    date: post[0].date.toISOString(),
+    changedDate: post[0].date.toLocaleDateString('en-CA', options)
+  }
+
+  res.render('post-detail', {
+    post: postData
   })
 })
 
